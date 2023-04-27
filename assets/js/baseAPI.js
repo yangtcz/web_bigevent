778a28dcc275a66a0cd0090bbeef7e29f652d5c4
ytcz <3144328668@qq.com>
2023-04-27 16:16:50 +0800

    完成了登录和注册功能的开发

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,7 @@
+//注意:每次调用$.get()或$.post()或$.ajax()的时候
+// 会先调用这个ajaxPrefilter函数
+// 这个函数中可以拿到Ajax提供的配置对象
+$.ajaxPrefilter(function(options) {
+    // 在发起真正的ajax之前统一拼接请求的根路径
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+})
